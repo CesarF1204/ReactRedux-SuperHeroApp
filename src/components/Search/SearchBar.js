771d88c69d9302c95ebacd731b63ecc3ec70dd50
Cesar Francisco <princexcesar@gmail.com>
2021-10-03 07:48:21 +0800

    updated codes with background and search keypress functionality

--- src/components/Search/SearchBar.js
+++ src/components/Search/SearchBar.js
@@ -4,25 +4,31 @@ import { Link } from 'react-router-dom';
 const SearchBar = (props) => {
 
     const [state, setState] = useState({
-    newSearch: ''
+        newSearch: ''
     });
 
     useEffect(()=>{
-    console.log('[SearchBar.js] Use Effect called!');
+        console.log('[SearchBar.js] Use Effect called!');
     }, [state.newSearch]);
 
     const inputNewTaskHandler = (event) => {
         setState({
-        newSearch: event.target.value,
+            newSearch: event.target.value,
         });
-    };
+    }
+
+    const onKeyEnteredSearch = (event) => {
+        if (event.key === "Enter") {
+            props.onSearchSuperHero(state.newSearch)
+        }
+    }
 
     return (
         <div>
             <nav>
                 <h1><Link className="link" to="/superhero/home">SuperHeroApp</Link></h1>
                 <div className="search">
-                    <input type="text" value={state.newSearch} onChange={inputNewTaskHandler} placeholder="Search..."></input>
+                    <input type="text" value={state.newSearch} onChange={inputNewTaskHandler} placeholder="Search..." onKeyPress={onKeyEnteredSearch}></input>
                     <button className="search-btn" onClick={() => props.onSearchSuperHero(state.newSearch)}>Search</button>
                 </div>
                 <h1><Link className="link" to="/superhero/selected">Favorites</Link></h1>
